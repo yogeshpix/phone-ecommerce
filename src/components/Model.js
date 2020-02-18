9c9bf929a10b72af0b8144c4c4b09a625a5f4db5
yogeshsi <yogeshsi@YOGESHSI-IN.us.oracle.com>
2020-02-19 01:06:38 +0530

    first commit

---
+++ src/components/Model.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import {ProductConsumer} from '../context';
+import {ButtonContainer} from './Button';
+import {Link} from 'react-router-dom';
+
+export class Model extends Component {
+    render() {
+        return (
+           <ProductConsumer>
+               {(value) => {
+                   const {modelOpen, closeModel} = value;
+                   const {img, title, price} = value.modelProduct;
+                   if(!modelOpen){
+                       return null;
+                   }
+                   else{
+                    return (   <ModelContainer>
+                           <div className="container">
+                               <div className="row">
+                                   <div id="model" className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize p-5">
+                                       <h5>item added to the cart</h5>
+                                       <img src={img} className="img-fluid" alt="Product"/>
+                    <h5>{title}</h5>
+                    <h5 className="text-muted">price : ${price}</h5>
+
+                    <Link to='/'>
+                        <ButtonContainer onClick={() => closeModel()}>
+                            Continue Shopping
+                        </ButtonContainer>
+
+                    </Link>
+                    <Link to='/cart'>
+                    <ButtonContainer cart onClick={() => closeModel()}>
+Go to Cart
+</ButtonContainer>
+                        </Link>
+                                   </div>
+                               </div>
+                           </div>
+                       </ModelContainer>);
+                   }
+               }}
+           </ProductConsumer>
+        )
+    }
+}
+
+export default Model
+
+
+const ModelContainer = styled.div`
+position:fixed;
+top:0;
+left:0;
+right:0;
+bottom:0;
+background:rgba(0,0,0,0.3);
+display: flex;
+align-items: center;
+justify-content: center;
+#model{
+    background: var(--mainWhite);
+}
+`;
